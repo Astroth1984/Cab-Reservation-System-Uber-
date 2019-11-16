f0c31005be8c549a33c05f5462ae79b5053a4c90
ElMounir_Othmane <elmounir.othmane666@gmail.com>
2019-11-16 01:07:46 +0100

    Project Architecture

---
+++ src/main/java/com/cab/reservation/system/CabReservationSysytemApplication.java
@@ -0,0 +1,13 @@
+package com.cab.reservation.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabReservationSysytemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabReservationSysytemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cab/reservation/system/CabReservationSysytemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cab.reservation.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabReservationSysytemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
